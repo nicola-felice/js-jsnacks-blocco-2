f15d3306d8783594ed579ba556145943fd6b0904
nicolafelice <felicenicola99@hotmail.com>
2021-08-27 13:06:10 +0200

    add repetition control on complete names

--- snack-3/app.js
+++ snack-3/app.js
@@ -7,10 +7,20 @@ const listaCognomi = [ 'verdi', 'rossi', 'bianchi', 'neri', 'carducci', 'gialli'
 // creo lista nomi
 const listaNomi = [ 'giacomo', 'marco', 'samuele', 'nicola', 'federico', 'nando', 'piero' ];
 
+// creo lista nomi completi generati
+const competeNames = [];
+
 // genero i tre nomi e cognomi e li stampo
-for (let i = 0; i < 3; i++) {
+while (competeNames.length < 3) {
     let generatedName = listaNomi[Math.floor(Math.random() * listaNomi.length)];
     let generatedSurname = listaCognomi[Math.floor(Math.random() * listaCognomi.length)];
 
-    console.log(generatedName, generatedSurname);
+    // if the complete name is not included add it to the list
+    if (!competeNames.includes(`${generatedName} ${generatedSurname}`)) {
+        competeNames.push(`${generatedName} ${generatedSurname}`);
+    }
+}
+
+for (let i = 0; i < competeNames.length; i++) {
+    console.log(competeNames[i]);
 }
